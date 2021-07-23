9a42e3e21be2f6ff6cec4d229e5bca4813547f43
Hripsime Tepelikian <htepelikian@gmail.com>
2021-07-23 10:34:48 -0700

    edit server js

--- server.js
+++ server.js
@@ -1,77 +1,99 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
+// Declare dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notes = require("./db/db.json");
+const uuid = require('./helpers/uuid');
 
-const express = require('express');
+//Port and express 
+const PORT = process.env.PORT || 3001
 const app = express();
 
-const allNotes = require('./db/db.json');
-
-app.use(express.urlencoded({ extended: true }));
+//Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
-
+//GET routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+ 
+// GET route to get the notes html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})  
+
+//GET note data
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
 });
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
 
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
+    console.info(`${req.method} request received to add new note`);
 
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
+    const { title, text } = req.body;
 
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
 
-            break;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            
+            const parsedNotes = JSON.parse(data);
+
+            parsedNotes.push(newNote);
+
+            fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) => 
+                writeErr ? console.error(err) : console.info('Successfully added note!'));
         }
+    });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting feedback');
     }
-}
+})
 
+// Delete function should work the same way as POST just need to filter out the selected note
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const uid = req.params.id;
+            const parsedNotes = JSON.parse(data);
 
+            let updatedNotes = parsedNotes.filter(notes => notes.id != uid);
+
+            fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, 4), (writeErr) =>
+                writeErr ? console.error(err) : console.info('Successfully deleted note!'));
+        }
+    });
+        const response = {
+            status: 'success',
+        };
+        res.json(response);
+})
+
+//Wildcard GET 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+  
+// PORT listener
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
